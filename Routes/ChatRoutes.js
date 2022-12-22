65c45ede92777885f074896936f483cc4c239755
Peter Chege <peterkagure@gmail.com>
2022-12-22 18:37:25 +0300

    changed import error 2

--- Routes/ChatRoutes.js
+++ Routes/ChatRoutes.js
@@ -1,8 +1,8 @@
 const Express = require("express")
 const router = Express.Router()
 
-const getChats = require("../controllers/ChatControllers/getChats");
-const getMessagesByUser = require("../controllers/ChatControllers/getChatMessages");
+const getChats = require("../Controllers/ChatControllers/getChats");
+const getMessagesByUser = require("../Controllers/ChatControllers/getChatMessages");
 
 router.get("/mychats/:email",getChats)
 router.get("/message/:senderEmail/:receiverEmail",getMessagesByUser)
